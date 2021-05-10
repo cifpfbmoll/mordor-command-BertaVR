b451bc28b9524496c455f68e8380f98e3961d223
BertaVR <bverges@cifpfbmoll.eu>
2021-05-10 21:44:34 +0200

    feat:  Pedido Nacional, casos test pasados hasta aquí

---
+++ mordor/src/main/java/edu/pingpong/pedidos/PedidoNacional.java
@@ -0,0 +1,30 @@
+package edu.pingpong.pedidos;
+import edu.pingpong.interfaces.Pedido;
+
+import java.util.UUID;
+
+public class PedidoNacional implements Pedido{
+    private final String id;
+    private String destino;
+    private int peso;
+
+    public PedidoNacional(String destino, int peso) {
+        this.id =  UUID.randomUUID().toString();
+        this.destino = destino;
+        this.peso = peso;
+    }
+
+    @Override
+    public int peso() {
+        return peso;
+    }
+
+    @Override
+    public String destino() {
+        return this.destino;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+}
